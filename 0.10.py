f11ce17cede95d9b40e911eaee8fb2ed6b1c9743
oliveriopt <oliver.pozo.m@gmail.com>
2026-03-03 17:07:27 -0300

    Create 0.10.py

---
+++ 0.10.py
@@ -0,0 +1,102 @@
+Dada estr header de sqlserver:
+OrderNumber	OrderKey	OrderDate	ScheduledPickupDate	ScheduledDeliveryDate	PickupDate	PickupLate	DeliveredDate	CarrierAssignDate	VoidDate	BusinessUnitCode	ServiceCode	SourceSystemName	FreightOptimizerCustomerCode	CarrierCode	EquipmentCode	OrderStatusTypeCode	BrokerageStatusTypeCode	OrderFinancialStatusTypeCode	IsOnHold	IsUncommited	TripCount	OrderSalesAmount	FuelChargeAmount	LineHaulChargeAmount	OrderCost	FuelCostAmount	LineHaulCostAmount	CrossBooked	BounceToBounce	OnTimeDelivery	DeliveryDelay	OnTimePickup	PickupDelay	BillDistance	WeightInPounds	OriginCity	OriginStateCode	OriginPostalCode	OriginCountryCode	OriginLocationID	DestinationCity	DestinationStateCode	DestinationPostalCode	DestinationCountryCode	DestinationLocationID	EnteredByUserName	PrimarySalesRepUserName	SecondarySalesRepUserName	BookedByUserName	PrimaryCarrierRepUserName	FreightAssignRepUserName	PrimaryCarrierRepLocationCode	CreatedByUser	DetentionAmount	TONUAmount	TotalAccessorialAmount	LHRate	IsHazmat	Quantity	Volume	PickupRescheduledReason	DropRescheduledReason	CarrierPortalPostingDate	IsDropAndHook	PickupServiceFailureCodeID	DeliveryServiceFailureCodeID	PercentageOfCommissionPrimary	DeliveryArrivalDate	OrderSourceCode	CompanyID	ValueOfGoods	BazookaOrderId	ShipmentID	PickupDepartureDate	PickupServiceFailureReasonCategoryID	DeliveryServiceFailureReasonCategoryID
+21498706	17399752	2026-01-28 08:34:34.69	2026-02-25 06:00:00.00	2026-02-26 06:00:00.00	2026-02-25 10:00:00.00	2026-02-25 20:00:00.00	2026-02-26 09:00:00.00	2026-01-28 08:52:30.10	NULL	LAK3	TL_CONT	Freight Optimizer	LIPPELI3	KIELWI	VN53	D	09DELVRD	NEW	N	N	0	755.30	81.90	673.40	700.00	0.00	700.00	1	0	1	NULL	1	NULL	182.00	40000.00	Altoona	WI	54720	USA	2740188	Appleton	WI	54911	USA	2699290	ckane	heimerman	NULL	bill.lerner	bill.lerner	NULL	COYGXCOV	ckane	0.00	0.00	0.00	3.70	0	24	NULL	NULL	NULL	NULL	0	NULL	NULL	100.00	2026-02-26 06:00:00.00	EDI	1	100000.0000	NULL	542344814	2026-02-25 11:00:00.00	NULL	NULL
+21127547	16442237	2026-01-16 13:40:59.76	2026-01-16 08:00:00.00	2026-01-19 15:30:00.00	2026-01-16 15:48:00.00	2026-01-16 15:30:00.00	2026-01-19 13:19:00.00	NULL	NULL	GDRY7	LTL	Freight Optimizer	LANDSA14	SECOSC	LTL	D	09DELVRD	COMPLETE	N	N	0	145.44	18.42	127.02	132.82	16.82	116.00	0	0	1	NULL	0	18	1.00	2055.00	Gonzales	TX	78629	USA	2635399	New Braunfels	TX	78130	USA	1517208	NULL	gbowman	NULL	vrep	vrep	NULL	NULL	vrep	0.00	0.00	0.00	127.02	0	1	NULL	NULL	NULL	NULL	0	73	73	100.00	2026-01-19 13:15:00.00	EDI-DOC	1	50.2000	NULL	12480151	2026-01-16 15:53:00.00	4	4
+22358482	19826354	2026-02-27 03:46:43.50	2026-03-09 11:11:00.00	2026-03-11 11:11:00.00	NULL	2026-03-09 12:12:00.00	NULL	NULL	NULL	CHA3	TL_CONT	Freight Optimizer	FIATCEM5	NULL	VN53	A	02AVAIL	NEW	Y	N	0	1996.05	336.48	1659.57	0.00	0.00	0.00	0	0	NULL	NULL	NULL	NULL	683.10	41842.00	Sterling Heights	MI	48312	USA	1167913	Gray Court	SC	29645	USA	1175611	svcAssistEdgeBot23	cpotter001	NULL	NULL	NULL	NULL	NULL	svcAssistEdgeBot23	0.00	0.00	0.00	1659.57	0	1	NULL	NULL	NULL	NULL	0	NULL	NULL	100.00	NULL	FO	1	100000.0000	NULL	22358482	NULL	NULL	NULL
+22377793	19844308	2026-03-02 12:06:14.41	2026-03-02 09:02:00.00	2026-03-03 11:02:00.00	2026-03-02 17:50:00.00	2026-03-02 11:02:00.00	NULL	NULL	NULL	CLT8	LTL	Freight Optimizer	ACUICHN4	OAKHAUWA	LTL	P	05PICKED	NEW	N	N	0	127.42	14.42	103.00	117.73	13.23	94.50	0	0	NULL	NULL	0	408	1.00	93.00	Ontario	CA	91761	USA	2787714	Santa Clara	CA	95050	USA	1975544	NULL	justin.lawing	NULL	vrep	vrep	NULL	NULL	vrep	0.00	0.00	10.00	103.00	0	6	NULL	NULL	NULL	NULL	0	NULL	NULL	100.00	NULL	EDI-DOC	1	0.0000	NULL	AYI00876412	2026-03-02 17:51:00.00	NULL	NULL
+22380743	19847099	2026-03-02 14:24:48.46	2026-03-06 13:00:00.00	2026-03-08 07:00:00.00	NULL	NULL	NULL	2026-03-03 10:10:36.96	NULL	GX5	TL_CONT	Freight Optimizer	SCJOCHI4	GOLDKITX	VN53	A	03ASSIGN	NEW	N	N	0	2613.52	256.52	2357.00	2300.00	0.00	2300.00	1	0	NULL	NULL	NULL	NULL	986.60	36489.00	Pleasant Prairie	WI	53158	USA	2389953	Fort Worth	TX	76177	USA	2669825	diego.gudino	matt.pokorny	NULL	mpacheco004	mpacheco004	NULL	XPOCLU	diego.gudino	0.00	0.00	0.00	2357.00	0	10668	NULL	Customer	Customer	NULL	0	NULL	NULL	100.00	NULL	EDI	14	100000.0000	NULL	202801240	NULL	NULL	NULL
+19362102	14793402	2025-06-16 18:57:54.59	2025-06-17 10:00:00.00	2025-06-17 17:00:00.00	2025-06-17 12:00:00.00	2025-06-17 17:00:00.00	2025-06-17 15:30:00.00	2025-06-16 21:43:35.07	NULL	PHX	TL_CONT	Freight Optimizer	TESLPACA	FASTPOFL	F48	D	09DELVRD	COMPLETE	N	N	0	430.66	20.66	310.00	305.00	0.00	305.00	0	0	1	NULL	1	NULL	62.60	5000.00	Sarasota	FL	34243	USA	1784795	Plant City	FL	33566	USA	1956234	zachary.dao	spetersen	NULL	phoenix.cannon	phoenix.cannon	NULL	XPOPHX	zachary.dao	0.00	0.00	100.00	310.00	0	1	NULL	NULL	NULL	NULL	0	NULL	NULL	100.00	2025-06-17 14:30:00.00	FO	1	100000.0000	NULL	SHP	2025-06-17 13:00:00.00	NULL	NULL
+22262214	19736276	2026-02-17 12:30:45.99	2026-03-02 08:00:00.00	2026-03-02 13:31:00.00	2026-03-02 01:52:41.00	2026-03-02 08:00:00.00	2026-03-02 16:00:29.00	2026-02-26 12:22:11.92	NULL	CDET2	TL_SPOT	Freight Optimizer	PEPPCHI5	OHIOTOOH	V	D	09DELVRD	NEW	N	N	0	752.78	38.81	713.97	750.00	0.00	750.00	0	0	1	NULL	1	NULL	83.00	26593.00	Napoleon	OH	43545	USA	2300881	BROWNSTOWN	MI	48193	USA	2348675	NULL	steve.ideh	NULL	josh.mason	josh.mason	NULL	COYGXCOV	BazDataMigration	0.00	0.00	0.00	713.97	0	5910	NULL	NULL	NULL	NULL	0	NULL	NULL	100.00	2026-03-02 12:44:23.00	BAZ	14	100000.0000	34121318	34121318	2026-03-02 11:06:54.00	NULL	NULL
+22342487	19811381	2026-02-25 15:47:11.22	2026-02-27 13:00:00.00	2026-03-02 23:59:00.00	2026-02-27 13:33:27.00	2026-02-27 15:00:00.00	2026-03-02 09:30:00.00	2026-02-26 07:51:30.18	NULL	CHA1	TL_CONT	Freight Optimizer	GENEMET1	TRANJOGA	VN53	D	09DELVRD	NEW	N	N	0	858.88	155.88	703.00	800.00	0.00	800.00	1	0	1	NULL	1	NULL	362.50	8125.00	Atlanta	GA	30336	USA	2674251	Memphis	TN	38118	USA	2634169	NULL	jvankleeck	NULL	apaternoster	apaternoster	NULL	XPOCMH	vrep	0.00	0.00	0.00	703.00	0	26	NULL	NULL	Customer	NULL	0	NULL	NULL	100.00	2026-03-02 09:00:00.00	EDI-DOC	1	100000.0000	NULL	N244944	2026-02-27 15:02:50.00	NULL	NULL
+22307384	19778615	2026-02-23 06:51:42.10	2026-02-23 15:00:00.00	2026-02-25 16:00:00.00	2026-02-23 15:30:00.00	2026-02-23 17:00:00.00	2026-02-25 11:12:00.00	NULL	NULL	CHA5	LTL	Freight Optimizer	MASOTAFL	SAIAJOGA	V	D	09DELVRD	PENDING	N	N	0	317.29	48.66	183.63	260.97	36.86	139.11	0	0	1	NULL	1	NULL	1.00	405.00	Pittsburg	KS	66762	USA	1889487	Indianapolis	IN	46278	USA	2798024	jafleischman	jafleischman	NULL	Connect CustPortal	Connect CustPortal	NULL	NULL	jafleischman	0.00	0.00	85.00	183.63	0	5	NULL	NULL	NULL	NULL	0	NULL	NULL	100.00	2026-02-25 11:11:00.00	XPOC	1	0.0000	NULL	NULL	2026-02-23 15:33:00.00	NULL	NULL
+21086885	16404042	2026-01-13 11:14:38.13	2026-01-14 09:00:00.00	2026-01-14 16:00:00.00	2026-01-14 08:47:00.00	NULL	2026-01-14 15:39:00.00	2026-01-13 14:35:40.63	NULL	GAI03	TL_CONT	Freight Optimizer	DRIVPON1	RISEFOPA	LS	D	09DELVRD	PROCESSING	N	N	0	729.00	0.00	729.00	600.00	0.00	600.00	0	0	1	NULL	1	NULL	205.70	3229.00	SOUTH BRUNSWICK	NJ	08810	USA	2501229	Sterling	VA	20166	USA	2687668	brivas	kmerck	NULL	clmartin	clmartin	NULL	XPOGAI2	brivas	0.00	0.00	0.00	729.00	0	10	NULL	NULL	Customer	NULL	0	NULL	NULL	100.00	2026-01-14 13:26:00.00	FO	1	100000.0000	NULL	PO1129072	2026-01-14 09:38:00.00	NULL	NULL
+
+en un archivo csv.
+
+EN un jsonl tenemos:
+[{
+  "source_system_name": "Freight Optimizer",
+  "order_source_code": "FO",
+  "business_unit_code": "SMBMPLS",
+  "service_type_code": "TL_SPOT",
+  "order_status_type_code": "D",
+  "brokerage_status_code": "09DELVRD",
+  "pickup_rescheduled_reason": null,
+  "drop_rescheduled_reason": null,
+  "is_on_hold": "N",
+  "is_uncommited": "N",
+  "order_financial_status_type_code": "COMPLETE",
+  "order_date": "2026-01-02T10:57:20.860000",
+  "scheduled_pickup_date": "2026-01-05T07:00:00",
+  "scheduled_delivery_date": "2026-01-06T13:00:00",
+  "pickup_date": "2026-01-05T11:45:53.970000",
+  "pickup_departure_date": "2026-01-05T13:00:00",
+  "delivered_date": "2026-01-06T08:09:25",
+  "delivery_arrival_date": "2026-01-06T07:56:20",
+  "carrier_assign_date": "2026-01-02T11:16:18.900000",
+  "void_date": null,
+  "freight_optimizer_customer_code": "2TOWCHIL",
+  "carrier_code": "KAMAYUCA",
+  "equipment_code": "VR",
+  "order_key": "16323978",
+  "order_number": "21001605",
+  "order_sales_amount": "1050",
+  "line_haul_charge_amount": "1050",
+  "fuel_charge_amount": "0",
+  "total_accessorial_charge": "0",
+  "detention_charge": "0",
+  "tonu_charge": "0",
+  "line_haul_pay_amount": "802.0",
+  "fuel_pay_amount": "0.0",
+  "order_cost": "802.0",
+  "lh_rate": "1050",
+  "trip_count": "0",
+  "bill_distance": "565.8",
+  "weight_in_pounds": "34711",
+  "on_time_delivery": "1",
+  "delivery_delay": null,
+  "on_time_pickup": "1",
+  "pickup_delay": null,
+  "origin_city": "Corvallis",
+  "origin_state_code": "OR",
+  "origin_postal_code": "97333",
+  "origin_country_code": "USA",
+  "origin_location_id": "2369003",
+  "destination_city": "Stockton",
+  "destination_state_code": "CA",
+  "destination_postal_code": "95215",
+  "destination_country_code": "USA",
+  "destination_location_id": "454716",
+  "entered_by_user_name": "bob.bulger",
+  "primary_sales_rep_user_name": "bob.bulger",
+  "secondary_sales_rep_user_name": null,
+  "booked_by_user_name": "sfiedor",
+  "primary_carrier_rep_user_name": "sfiedor",
+  "freight_assign_rep_user_name": null,
+  "dispatch_office_location_code": "XPOCH3",
+  "cross_booked": "1",
+  "bounce_to_bounce": "0",
+  "created_by_user": "bob.bulger",
+  "is_hazmat": "false",
+  "quantity": "20",
+  "pickup_late": "2026-01-05T15:30:00",
+  "carrier_portal_posting_date": null,
+  "is_drop_and_hook": "false",
+  "pickup_service_failure_code_id": null,
+  "pickup_service_failure_reason_category_id": null,
+  "delivery_service_failure_code_id": null,
+  "delivery_service_failure_reason_category_id": null,
+  "percentage_of_commission_primary": "100.0",
+  "company_id": null,
+  "value_of_goods": "100000",
+  "bazooka_order_id": null,
+  "shipment_id": "DP-12136",
+  "origin_scheduled_departure_time": null,
+  "destination_stop_scheduled_departure_time": null,
+  "trip_id": "16426220",
+  "trip_brokerage_status_id": "8",
+  "carrier_id": "427107"
+},
+
+ Quiero que hagas un script en python para verificar cuantos OrderKey estan en order_key. LOs archivos estan en la misma carpeta que python script, pero para evitar problemas con el compilador usa PATH.
+ 
